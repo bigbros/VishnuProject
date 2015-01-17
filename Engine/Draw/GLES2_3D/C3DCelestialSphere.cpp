3a13abbc66a6e947d14e83421fde43df91bd54de
Yoshihito Kira <bigbros@madtape.com>
2015-01-17 18:00:23 +0900

    天球描画用ソース追加(中身はまだ。やる気でたら書く)

--- Engine/Draw/GLES2_3D/C3DCelestialSphere.cpp
+++ Engine/Draw/GLES2_3D/C3DCelestialSphere.cpp
@@ -1,2 +1,55 @@
 #include "C3DCelestialSphere.h"
 
+C3DCelestialModel::C3DCelestialModel(float r, const char * modelName)
+{
+
+}
+
+C3DCelestialModel::~C3DCelestialModel()
+{
+
+}
+
+void
+C3DCelestialModel::setup(C3DDrawEnv * env)
+{
+
+}
+
+void
+C3DCelestialModel::cleanup(C3DDrawEnv * env)
+{
+
+}
+
+bool
+C3DCelestialModel::recovery()
+{
+
+}
+
+bool
+C3DCelestialModel::destruction()
+{
+
+}
+
+
+bool
+C3DCelestialModel::newVertices(int num = 0)
+{
+
+}
+
+
+bool
+C3DCelestialModel::newIndices(int num = 0)
+{
+
+}
+
+void
+C3DCelestialModel::setBuffer()
+{
+
+}
