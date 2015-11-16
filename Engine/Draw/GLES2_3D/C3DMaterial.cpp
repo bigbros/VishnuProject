6880842c52c3239ae3c9b7f30b19df09ac7977b1
Yoshihito Kira <bigbros@madtape.com>
2015-11-17 01:48:50 +0900

    error handling (3)

--- Engine/Draw/GLES2_3D/C3DMaterial.cpp
+++ Engine/Draw/GLES2_3D/C3DMaterial.cpp
@@ -30,16 +30,16 @@ C3DMaterial::setTexture(int mask, CGLTex * tex, GLenum target, int num, GLint sh
 	else {
 		glUniform1i(flagIdx, GL_FALSE);
 	}
-	GLenum errcode;
-	while (errcode = glGetError()) {
-		if(errcode == GL_INVALID_OPERATION) LOG("GL_INVALID_OPERATION in C3DMaterial::setTexture().\n");
-	}
-
 }
 void
 C3DMaterial::setup(C3DDefaultShader * shader)
 {
-	setTexture(TEXTURE, m_texture,	GL_TEXTURE0, 0, shader->m_u_maptexture, shader->m_u_f_texmap);
+	GLenum errcode;
+	while (errcode = glGetError()) {
+		if (errcode == GL_INVALID_OPERATION) LOG("GL_INVALID_OPERATION in C3DMaterial::setTexture().\n");
+	}
+
+	setTexture(TEXTURE, m_texture, GL_TEXTURE0, 0, shader->m_u_maptexture, shader->m_u_f_texmap);
 	setTexture(NORMAL, m_normal,	GL_TEXTURE1, 1, shader->m_u_mapnormal, shader->m_u_f_normalmap);
 	setTexture(SPECULAR, m_specular, GL_TEXTURE2, 2, shader->m_u_mapspecular, shader->m_u_f_specularmap);
 
